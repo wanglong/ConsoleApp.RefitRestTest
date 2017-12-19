2b6258d8c04d5fb15b107558646c7fd5cc7aba60
HackerVirus <nmgimust_wl@163.com>
2017-12-19 17:52:24 +0800

    test

---
+++ ConsoleApp.RefitRestTest/IGitHubApi.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.RefitRestTest
+{
+    public interface IGitHubApi
+    {
+        [Get("/users/{user}")]
+        Task<User> GetUserAsync(string user);
+    }
+}
--- ConsoleApp.RefitRestTest/Program.cs
+++ ConsoleApp.RefitRestTest/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Refit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp.RefitRestTest
 {
@@ -6,7 +9,56 @@ namespace ConsoleApp.RefitRestTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Main Thread Id: {0}\r\n", Thread.CurrentThread.ManagedThreadId);
             Console.WriteLine("Hello World!");
+
+            Test();   
+
+            //Test1();
+
+            Console.ReadLine();
+        }
+
+        static async Task Test1()
+        {
+            Console.WriteLine("Before calling GetUser, Thread Id: {0}\r\n", Thread.CurrentThread.ManagedThreadId);
+            var userInfo = GetAsync();    //我们这里没有用 await,所以下面的代码可以继续执行
+                                          // 但是如果上面是 await GetAsync()，下面的代码就不会立即执行，输出结果就不一样了。
+            Console.WriteLine("End calling GetUser.\r\n");
+            var octocat = await userInfo;
+            Console.WriteLine("Get result from GetUser octocat: {0}", octocat.Id +"  "+ octocat.Name);            
+        }
+
+        static async Task<User> GetAsync()
+        {   // 这里还是主线程
+            Console.WriteLine("Before calling Task.Run, current thread Id is: {0}", Thread.CurrentThread.ManagedThreadId);
+            var gitHubApi = RestService.For<IGitHubApi>("https://api.github.com");
+            var octocat = await gitHubApi.GetUserAsync("octocat");
+            Console.WriteLine("'GetUser' Thread Id: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Get result from GetUser octocat: {0}", octocat.Id + "  " + octocat.Name);
+            return octocat;
+        }
+
+
+        static async Task Test()
+        {
+            Console.WriteLine("Before calling GetName, Thread Id: {0}\r\n", Thread.CurrentThread.ManagedThreadId);
+            var name = GetName();   //我们这里没有用 await,所以下面的代码可以继续执行
+                                    // 但是如果上面是 await GetName()，下面的代码就不会立即执行，输出结果就不一样了。
+            Console.WriteLine("End calling GetName.\r\n");
+            Console.WriteLine("Get result from GetName: {0}", await name);
+        }
+
+        static async Task<string> GetName()
+        {
+            // 这里还是主线程
+            Console.WriteLine("Before calling Task.Run, current thread Id is: {0}", Thread.CurrentThread.ManagedThreadId);
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("'GetName' Thread Id: {0}", Thread.CurrentThread.ManagedThreadId);
+                return "Jesse";
+            });
         }
     }
 }
